331841f6060144d3584794ad743348327eb5659e
Carlos Soria <soriac367@gmail.com>
2022-09-19 12:36:42 -0500

    Added status endpoint

---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+
+from flask import jsonify
+from api.v1.views import app_views
+
+
+
+@app_views.route('/status')
+def status():
+    """Returs the status of the service"""
+    return jsonify({"status": "OK"})
