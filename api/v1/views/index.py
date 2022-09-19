8ccbb53f7502881a560af7c80d4a3471c1029956
Carlos Soria <soriac367@gmail.com>
2022-09-19 13:36:38 -0500

    Task5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,30 @@
 """Main enpoints"""
 from flask import jsonify
 from api.v1.views import app_views
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 
 
 @app_views.route('/status')
 def status():
     """Returns the status of the service"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    """Returns all categories and their quantity"""
+
+    return (jsonify({
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }))
