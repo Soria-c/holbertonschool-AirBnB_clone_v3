505e87bbd61a1d927cbce8a7539966636c3e9e2a
Carlos Soria <soriac367@gmail.com>
2022-09-19 13:39:07 -0500

    Task6

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Main"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -15,6 +15,12 @@ app.register_blueprint(app_views)
 def close(self):
     "Closes the db connection"
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(e):
+    """Handles the 404 error"""
+    return jsonify({'error': 'Not found'}), 404
 
 
 if __name__ == '__main__':
