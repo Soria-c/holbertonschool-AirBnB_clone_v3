fc1894646176ad25c33240f2be99cb690ccae0a7
Carlos Soria <soriac367@gmail.com>
2022-09-19 12:42:18 -0500

    Fixed style

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,14 +10,14 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def close():
     "Closes the db connection"
     storage.close()
 
 
-
 if __name__ == '__main__':
     ip = getenv('HBNB_API_HOST')
     port = getenv('HBNB_API_PORT')
-    app.run(host=ip or '0.0.0.0' , port=port or 5000, threaded=True)
+    app.run(host=ip or '0.0.0.0', port=port or 5000, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,7 @@
-
 #!/usr/bin/python3
 
 from flask import jsonify
 from api.v1.views import app_views
-
 
 
 @app_views.route('/status')
